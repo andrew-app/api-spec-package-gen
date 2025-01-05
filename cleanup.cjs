e4370e9d9b8523dbb3478bbe1674e13a034e59cf
andrew-app <54277779+andrew-app@users.noreply.github.com>
2025-01-06 03:03:21 +1100

    remove pre and post scripts

--- cleanup.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('node:fs');
-
-fs.rm('node_modules', { recursive: true, force: true }, err => {
-  if (err) {
-    throw err;
-  }
-});
--- setup.cjs
+++
@@ -1,3 +0,0 @@
-const { exec } = require('node:child_process');
-
-exec('npm i');
